88ad2ff7741517370f316835320474f19a19fbbc
Changyuan Lin <coreballium@outlook.com>
2019-04-24 19:30:46 -0600

    First commit

---
+++ desktop/src/coreball/notninjacats/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package coreball.notninjacats.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import coreball.notninjacats.NotNinjaCats;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new NotNinjaCats(), config);
+	}
+}
